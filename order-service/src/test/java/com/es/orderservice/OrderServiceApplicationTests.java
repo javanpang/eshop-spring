d63b9a012234b0093a6fc4d3dbca199abb22e533
javanpang <javanpang@hotmail.com>
2026-06-08 01:08:02 +0100

    feat: initialise order-service

---
+++ order-service/src/test/java/com/es/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.es.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
